e3480635350d98a76bc0be2370920a2db1d4a779
Ronando Long <ronando.long@gmail.com>
2015-12-09 14:21:11 -0800

    It will show images using various fading and sliding elements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
     $("#walrus .clickable").click(function() {
-        $("#walrus .image-showing").toggle();
-        $("#walrus .image-hidden").toggle();
+        $("#walrus .image-showing").fadeToggle(1500);
+        $("#walrus .image-hidden").fadeToggle();
     });
 
     $("#alien .clickable").click(function() {
-        $("#alien .image-showing").toggle();
-        $("#alien .image-hidden").toggle();
+        $("#alien .image-showing").slideToggle(700);
+        $("#alien .image-hidden").slideToggle();
     });
 
     $("#batman .clickable").click(function() {
-        $("#batman .image-hidden").toggle();
-        $("#batman .image-showing").toggle();
+        $("#batman .image-hidden").slideToggle(200);
+        $("#batman .image-showing").slideToggle();
     });
 
     $("#halo .clickable").click(function() {
-        $("#halo .image-hidden").toggle();
-        $("#halo .image-showing").toggle();
+        $("#halo .image-hidden").slideToggle();
+        $("#halo .image-showing").slideToggle();
     });
 
     $("#stormtrooper .clickable").click(function() {
